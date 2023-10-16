c703993cede45e6ac896e7d0de765199434fa45e
KkarolaS <133031923+KkarolaS@users.noreply.github.com>
2023-10-16 07:39:02 +0200

    add function to button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [itemsList, setItemsList] = useState([]);
+
+  const [item, setItem] = useState("");
+
+  const handleChange = (e) => {
+    const newValue = e.target.value;
+    setItem(newValue);
+  };
+
+  const addItem = () => {
+    setItemsList((prevValue) => [...prevValue, item]);
+    setItem("");
+  };
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleChange} type="text" value={item} />
+        <button onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          {itemsList.map((item) => (
+            <li key={item}>{item}</li>
+          ))}
         </ul>
       </div>
     </div>
